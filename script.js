ebf3f97e6b62dec26f7f0bac3ff72f23a887c0f3
sahiljo14 <sahil140106@gmail.com>
2025-04-04 18:38:16 +0530

    On branch main  Changes to be committed: 	modified:   index.html 	new file:   script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+var typed = new Typed('#element', {
+    strings: ['an Electronics Student.', 'a Tech Enthusiast.', 'a Software Developer.'],
+    typeSpeed: 70,
+});
+
+function reveal() {
+    var reveals = document.querySelectorAll(".secondSection, .ThirdSection, .fourthSection, .fifthSection");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("show");
+        } else {
+            reveals[i].classList.remove("show");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
